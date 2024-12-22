b55bf85cc421b78b098f13fcd1f9fea6d9fd8dfa
jo.sh.in <jshin02@uw.edu>
2024-12-21 23:45:20 -0800

    adjusted js file

--- script.js
+++ script.js
@@ -2,7 +2,5 @@ fetch('content/header.json')
   .then(response => response.json())
   .then(data => {
     document.getElementById('site-title').textContent = data.site_title;
-    document.getElementById('phone-number').textContent = `Phone: ${data.phone_number}`;
-    document.getElementById('hours').textContent = `Hours: ${data.hours}`;
   })
   .catch(error => console.error('Error loading content:', error));
